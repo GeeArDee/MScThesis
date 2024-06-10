ec55fd1b6c353dac268284ff96d816a5da9d548e
GeeArDee <130880275+GeeArDee@users.noreply.github.com>
2024-06-10 16:31:58 -0400

    Lit review and Saad update

--- code/Saad_TankBlowdown.py
+++ code/Saad_TankBlowdown.py
@@ -6,28 +6,33 @@ import pandas as pd
 # import data from excel and plot experimental data
 
 start_time = 54.85
-stop_time = 55.38
+stop_time = 55.85
 
-df = pd.read_excel('C:/Users/gdub5/OneDrive/McGill/Thesis/Experimental things/LSP V2 WinDAQ data/LSP315_V2_Flow4.xlsx', usecols='A, L')
+P_atm = 101325   # Pa
+P_ini = 18.96*1e5 + P_atm # Pa, Initial pressure in absolute
+start_press = 0  # Bar
+stop_press = -(P_ini/1e5)*0.528  # Bar
+
+df = pd.read_excel('C:/Users/gdub5/OneDrive/McGill/Thesis/Experimental things/LSP V2 WinDAQ data/LSP315_V2_Flow4.xlsx', usecols='A, K, L')
 plt.figure(1)
 df_cut = df.loc[(df['Relative Time'] > start_time) & (df['Relative Time'] < stop_time)]
 x_exp = df_cut['Relative Time']-start_time # s
 y_exp = df_cut['Bar PCB'] # bar
-plt.errorbar(x_exp, y_exp, yerr=-y_exp*0.01, label='Experimental Data from PCB transducer', fmt='.') # plot in bar cool
+y_exp_Omega = df_cut['Bar Omega']
+plt.errorbar(x_exp, y_exp+P_ini/1e5, yerr=-y_exp*0.01, label='Experimental Data from PCB transducer', fmt='.') # plot in bar
+plt.scatter(x_exp-0.02, y_exp_Omega, color='black', marker='.', label='Experimental Data from Omega transducer')
 plt.xlabel('Time (s)')
 plt.ylabel('Pressure (Bar)')
 plt.title('Saad Tank Blowdown Curve vs Experimental')
 plt.grid(which='both')
 
 # Initialize arrays and values
-start_press = 0  # Bar
-stop_press = -7 # Bar
+
 P = np.linspace(start_press, stop_press, num=1000)*1e5
-P_atm = 101325   # Pa
-P_ini = 18.96*1e5 + P_atm # Pa, Initial pressure in absolute
+
 V = 9.68*10**-6 # Thruster volume in m^3, ### should includes volume of tubing??
 
-diam_list = [0.2,0.3,0.35]  # throat diameter, in mm
+diam_list = [0.39,0.4, 0.41]  # throat diameter, in mm
 
 D = np.array(diam_list)*10**-3 # throat diameter, in m
 A = np.pi*(D/2)**2
@@ -48,7 +53,7 @@ for i, p in enumerate(P):
     time[i] = sol_isentropic(x, V, A, T, gamma, R)#+start_time+0.03
 
 plt.figure(1)
-plt.plot(time, P/1e5 - 0.52664, label=diam_list) # plot in bar
+plt.plot(time, P/1e5 - 0.52664+P_ini/1e5, label=diam_list) # plot in bar
 plt.legend()
 
 # %%
