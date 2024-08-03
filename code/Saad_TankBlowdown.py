9ba6ee8806c356134c6f660e3cfc74ee3b46477f
GeeArDee <130880275+GeeArDee@users.noreply.github.com>
2024-08-03 19:21:04 -0400

    work

--- code/Saad_TankBlowdown.py
+++ code/Saad_TankBlowdown.py
@@ -8,9 +8,9 @@ import pandas as pd
 start_time = 54.85
 stop_time = 57.85
 
-P_atm = 101325   # Pa
-P_ini = 19e5 #18.96e5 + P_atm # Pa, Initial pressure in absolute
-P_stop = P_atm/0.528  # Pa, When 
+P_atm = 101325e-5   # Pa
+P_ini = 20e5 #18.96e5 + P_atm # Pa, Initial pressure in absolute
+P_stop = P_atm*1e5/0.528  # Pa, When 
 
 df = pd.read_excel('C:/Users/gdub5/OneDrive/McGill/Thesis/Experimental things/LSP V2 WinDAQ data/LSP315_V2_Flow4.xlsx', usecols='A, K, L')
 plt.figure(1)
@@ -19,16 +19,16 @@ x_exp = df_cut['Relative Time']-start_time # s
 y_exp = df_cut['Bar PCB'] # bar
 y_exp_Omega = df_cut['Bar Omega']
 # plt.errorbar(x_exp, y_exp+P_ini/1e5, yerr=-y_exp*0.01, label='Experimental Data from PCB transducer', fmt='.') # plot in bar
-plt.scatter(x_exp-0.02, y_exp_Omega, color='black', marker='.', label='Experimental Data from Omega transducer')
+plt.scatter(x_exp-0.02, y_exp_Omega + P_atm , color='black', marker='.', label='Experimental data')
 plt.xlabel('Time (s)')
-plt.ylabel('Pressure (Bar)')
-plt.title('Saad Tank Blowdown Curve vs Experimental')
-plt.grid(which='both')
+plt.ylabel('Absolute Pressure (bar)')
+#plt.title('Saad Tank Blowdown Curve vs Experimental')
+#plt.grid(off)
 
 # Initialize arrays and values
 P = np.linspace(P_ini, P_stop, num=1000)
 V = 9.68*10**-6 # Thruster volume in m^3, ### should includes volume of tubing??
-diam_list = [0.21,0.23,0.2,0.3]  # throat diameter, in mm
+diam_list = [0.20,0.21,0.22]  # throat diameter, in mm
 D = np.array(diam_list)*10**-3 # throat diameter, in m
 A = np.pi/4*D**2
 T = 300 # K
@@ -47,7 +47,11 @@ for i, p in enumerate(P):
     time[i] = sol_isentropic(x, V, A, T, gamma, R)
 
 plt.figure(1)
-plt.plot(time, P/1e5, label=diam_list) # plot in bar
+plt.plot(time+0.01, P/1e5, label=diam_list) # plot in bar
 plt.legend()
+#plt.ylim((0,22.5))
+#plt.xlim((0,3))
+
+plt.savefig('Thesis/assets/4 experiments/Saad blowdown fit.pdf')
 
 # %%
