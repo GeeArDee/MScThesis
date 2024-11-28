fac43fcc9bf047c1320b8b2637ee5c2e4a95aab5
GeeArDee <130880275+GeeArDee@users.noreply.github.com>
2024-11-27 19:48:02 -0500

    0D model advancement

--- code/Equilibrium calculations/LSP 0D model.py
+++ code/Equilibrium calculations/LSP 0D model.py
@@ -6,104 +6,84 @@ from scipy.optimize import minimize, Bounds
 import pandas as pd
 import sympy as sp
 from labellines import labelLines
-from tabulate import tabulate
 
 
 # Initialize variables
-t = 0                                       # time (s)
-delta_t = 1/1000                            # timestep of the simulation (s)
-t_end = 10                                  # end time of simulation (s)
-t_lsr_on = 0                                # time when laser is turned on (s)
-t_lsr_off = 10/1000                         # time when laser is shut off (s)
+t = 0                                       # Time (s)
+t_end = 10                                  # End time of the simulation (s)
+n_points = 1000                             # Number of points for radiation simulation (-)
 
-T_plasma = 15000                            # Temperature of the plasma (K)
 T_ini = 300                                 # Initial temperature of the chamber (K)
-P_test = 20e5                               # Pressure of the chamber (20 bar to Pa)
-lsr_power = 3000                            # Laser pulse power (W)
+p_ini = 20e5                                # Pressure of the chamber (20 bar to Pa)
 n_absorp_lsr = 0.8                          # Efficiency of laser absorption by the plasma cone (-)
-delta_q = lsr_power*(t_lsr_off-t_lsr_on)    # Total input energy by the laser (s)
+E_laser = 30                                # Total input energy by the laser (J)
 V_chamberV1 = 0.4/1000                      # Volume of the V1 chamber (0.4 L to m^3)
+rho_argon_300K =   20*1.784                         # Density of argon at 300 K (kg/m^3)
 
-# Load Cp from saved file (from Variable Cp.py)
-Cp_20bar_array = np.ndarray.flatten(np.load('Cp_20bar.npy'))    # Cp of argon at 20 bar, depends on temperature (kJ/kg-K)
-T_array = np.load('T_20bar.npy')            # Temperature array going with Cp (K)
-#plt.figure()
-#plt.plot(T_array, Cp_20bar_array)
-print(T_array)
-print(Cp_20bar_array)
 
-# Knowing cp from Equilibrium, find mass and volume of plasma ball, assuming constant pressure
+# Functions
+def rho_300K(T, p):
+    R_u = 8314.462                          # Universal gas constant [J/kmol-K]
+    MW_Ar = 39.948                          # Molecular weight of argon [u]
+    R_specific = R_u/MW_Ar
+    return p/(R_specific*T)
 
-# Volume of plasma cone
-l = 2/100   # length of plasma cone (m)
-r = 1/1000  # radius of the base of plasma cone (m) CHANGE THIS, FIRST APPROX.
-V_plasma = np.pi*r**2*l/3#*(t/t_lsr_off)  # Volume of the plasma cone (m^3) grows linearly with time, reaching max volume at t_lsr_off
+print(rho_300K(300, 20e5))
 
-# %% HW problem state 3 with variable c_p
 def p(m, R_specific, T, V):
     #  p V = m R_s T
     return m*R_specific*T/V
 
-def T(Q, m, c, T_0):
-    #  Q = m c delta_T
-    return Q/(m*c)+T_0
+# def T_final(Q, m, T_1, p_1, p_2):
+#     # Q = m * (h2 - h1), solve for T_2
+#     h1 = 
+#     ...
+#     return 
 
-V_0 = 0.0004  # m^3
-MW_Ar = 39.95  # kg/kmol
-p_0 = 20e5  # Pa
-T_0 = 300  # K
-R_u = 8314.462  # J/K-kmol
-R_Ar = R_u/MW_Ar
-cv_300K = 3/2 * R_Ar
-cp_300K = 5/2 * R_Ar
-k_300K = cp_300K/cv_300K
-Q = 0.8*30  # J
-V_A0 = 1/1000*V_0
+def V_final(p, N_ions, N_electrons, T):
+    # p*V = N*R_u*T
+    R_u = 8314.462                          # Universal gas constant [J/kmol-K]
+    N = N_ions + N_electrons
+    return N*R_u*T/p
 
-# STATE 0 (Initial)
-V_B0 = V_0-V_A0
-m_A = p_0*V_A0/(R_Ar*T_0)
-m_tot = p_0*V_0/(R_Ar*T_0)
-m_B = m_tot-m_A
 
-# STATE 3 (Heat addition via constant pressure)
-# Iterate to find right variable cp
-T_ini = 15000  # First guess
-delta = 10
-delta_max = 1  # Maximum allowable delta
+# STEP 1: Volume and mass of plasma cone at p_ini and T_ini 
+l = 2/100                                   # Length of plasma cone (m)
+r = 1/1000                                  # Radius of the base of plasma cone (m) CHANGE THIS, FIRST APPROX.
+V_plasma = np.pi*r**2*l/3                   # Volume of plasma cone (m^3)
+m_plasma = V_plasma*rho_argon_300K          # Mass of plasma (kg), will be taken as constant during this exercise
+print(m_plasma)
 
-while delta > delta_max:
-    cp_var = np.interp(T_ini, T_array, Cp_20bar_array)
-    print(cp_var)
-    T_A3 = T(Q, m_A, cp_var, T_0)
-    delta = abs(T_ini - T_A3)
-    T_ini = T_A3
+# STEP 2: Add 30 J of energy to m_plasma, while keeping constant pressure
+p_2 = p_ini                                             # Plasma cone is at the same pressure as the surrounding gas
+T_2 = T_final(E_laser, m_plasma, T_ini, p_ini, p_2)     # Temperature of plasma after energy addition (K)
 
-p_A3 = p_0
-V_A3 = m_A*R_Ar*T_A3/p_A3
-V_B3 = V_0 - V_A3
-p_B3 = p_0*V_B0**k_300K/(V_B3**k_300K)
-T_B3 = p_B3*V_B3/(m_B*R_Ar)
 
-#  STATE 4 (Uniform heat addition to entire volume)
-T_4 = T(Q, m_tot, cv_300K, T_0)
-p_4 = p(m_tot, R_Ar, T_4, V_0)
+# STEP 3: Find new volume (V_2) of cone
+V_2 = V_final(p_2, N_ions, N_electrons, T_2)
+print(V_2)
 
-#  Printing
-table = [['STATE', 'p [kPa]', 'T [K]'],
-            ['A3', p_A3/1000, T_A3],
-            ['B3', p_B3/1000, T_B3],
-            ['4', p_4/1000, T_4]]
-print(V_A0)
-print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
+# STEP 4: Calculate pressure increase in chamber due to expansion of gas in plasma core
+
+
+# STEP 5: As gas cools due to Brems. radiation, volume of the cone contracts
 
 # %% Next steps
 
-#while t < t_end:
+#while t < t_end:#
 
 # Calculate rate of heat loss via bremsstralung.
 # Is plasma ball optically thick or thin? -> volumetric/surface emitter
 # Rate of heat loss? Suppose it's thick (a surface emitter)?
 # delta_q = m*(h2-h1)
 
+# %% Old code
+
+# # Load Cp from saved file (from Variable Cp.py)
+# Cp_20bar_array = np.ndarray.flatten(np.load('Cp_20bar.npy'))    # Cp of argon at 20 bar, depends on temperature (kJ/kg-K)
+# T_array = np.load('T_20bar.npy')            # Temperature array going with Cp (K)
+# #plt.figure()
+# #plt.plot(T_array, Cp_20bar_array)
+# print(T_array)
+# print(Cp_20bar_array)
 
